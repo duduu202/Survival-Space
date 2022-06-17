a51c335145c8d5d5fd65a000028e70fc94a5ea35
duduu202 <95447103+duduu202@users.noreply.github.com>
2022-06-17 02:55:08 -0300

    Delete SurvivalSpace directory

--- SurvivalSpace/src/GameInput/Input.java
+++
@@ -1,7 +0,0 @@
-package GameInput;
-
-
-//NÃO É MAIS USADO, FOI SUBSTITUIDO POR CODIGOS DAS TECLAS
-public enum Input {
-    LEFT, RIGHT, UP, DOWN, ZERO, SHIFT;
-}
--- SurvivalSpace/src/GameInput/InputHandler.java
+++
@@ -1,7 +0,0 @@
-package GameInput;
-
-public interface InputHandler {
-    public void add(Input input);
-    public void remove(Input input);
-    //public boolean isPressed(Input input);
-}
